0590dc6f086b9e546badc0ac1a3d12d0bfc181f3
Matthew Lowdon-Elia <matt.lowdon@btinternet.com>
2026-09-04 14:39:53 +0100

    Working on DB connections

--- go/main.go
+++ go/main.go
@@ -4,10 +4,14 @@ import (
 	"database/sql"
 	"log/slog"
 	"net/http"
+	"net/url"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/lib/pq"
+
+	"example.com/recipe-storage/internal/config"
 )
 
 type recipe struct {
@@ -19,7 +23,29 @@ type recipe struct {
 }
 
 func main() {
-	db, err := sql.Open("postgres", "postgres://postgres_db:5432/recipes")
+	cfg := config.LoadConfig()
+
+	dbConfig = pq.Config{
+		Host : cfg.DB.Host,
+		Port : cfg.DB.Port,
+		User: cfg.DB.Port,
+		Password: cfg.DB.Password,
+
+	// pqConf = pq.Config(
+	// 	url
+	// )
+
+	db, err := sql.Open("postgres", "postgres://postgres_db:5434/recipes")
+
+	if err != nil {
+		// Return is only for local development
+		slog.Error("Failed to open connection", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	// Testing db connection
+
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
@@ -30,6 +56,25 @@ func main() {
 	http.ListenAndServe(":3000", router)
 }
 
-func getRecipes(w http.ResponseWriter, r *http.Request) {
+func getRecipes(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 
+}
+
+func connectToDb(config pq.Config) (*sql.DB, error) {
+	slog.Info("Connecting to database")
+	c, err := pq.NewConnectorConfig(config)
+
+	if err != nil {
+		slog.Error("pq connector error", "error", err)
+		return nil, err
+	}
+
+	db := sql.OpenDB(c)
+	err = db.Ping()
+	if err != nil {
+		slog.Error("Database connection error", "error", err)
+		return nil, err
+	}
+
+	return db, nil
 }
