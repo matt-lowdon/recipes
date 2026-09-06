fa4a7a873640c077ff15eac3ccbc098d4ca49c01
Matthew Lowdon-Elia <matt.lowdon@btinternet.com>
2026-09-06 16:33:50 +0100

    Shifted ordering, added a RecipeRequest struct to handle request payloads

--- go/main.go
+++ go/main.go
@@ -2,58 +2,67 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"log/slog"
 	"net/http"
-	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 	"github.com/lib/pq"
-
-	"example.com/recipe-storage/internal/config"
+	// "example.com/recipe-storage/internal/config"
 )
 
-type recipe struct {
-	Name         string
-	Ingredients  []string
-	Instructions []string
-	CookingTime  int64
-	PeopleCount  int64
-}
-
 func main() {
-	cfg := config.LoadConfig()
+	// cfg := config.LoadConfig()
 
-	dbConfig = pq.Config{
-		Host : cfg.DB.Host,
-		Port : cfg.DB.Port,
-		User: cfg.DB.Port,
-		Password: cfg.DB.Password,
+	// dbConfig = pq.Config{
+	// 	Host : cfg.DB.Host,
+	// 	Port : cfg.DB.Port,
+	// 	User: cfg.DB.Port,
+	// 	Password: cfg.DB.Password,
 
-	// pqConf = pq.Config(
-	// 	url
-	// )
-
-	db, err := sql.Open("postgres", "postgres://postgres_db:5434/recipes")
-
+	// ParseUint returns unit64, which needs converting for pq.Config
+	port, err := strconv.ParseUint(os.Getenv("PORT"), 10, 16)
 	if err != nil {
-		// Return is only for local development
-		slog.Error("Failed to open connection", "error", err)
+		slog.Warn("No port env var found. Defaulting to 5434")
+		port = uint64(5434)
+	}
+
+	pqConf := pq.Config{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     uint16(port),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Database: os.Getenv("POSTGRES_DB"),
+	}
+
+	db, err := connectToDb(pqConf)
+	if err != nil {
 		os.Exit(1)
 	}
-	defer db.Close()
 
-	// Testing db connection
+	defer db.Close()
 
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
 
+	router.Post("/recipes", postRecipes)
+
 	http.ListenAndServe(":3000", router)
+}
+
+func postRecipes(w http.ResponseWriter, r *http.Request) {
+	data := &RecipeRequest{}
+	if err := render.Bind(r, data); err != nil {
+	}
 }
 
 func getRecipes(w http.ResponseWriter, r *http.Request, db *sql.DB) {
@@ -77,4 +86,24 @@ func connectToDb(config pq.Config) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+type Recipe struct {
+	Name         string   `json:"name"`
+	Ingredients  []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	CookingTime  int64    `json:"cooking_time"`
+	PeopleCount  int64    `json:"people_count"`
+}
+
+type RecipeRequest struct {
+	*Recipe
+}
+
+func (a *RecipeRequest) Bind(r *http.Request) error {
+	if a.Recipe == nil {
+		return errors.New("Missing required Recipe fields")
+	}
+
+	return nil
 }
