18b65a9e646cbc38a5bcc0a943f6aa6d9be6c69a
Matthew Lowdon-Elia <matt.lowdon@btinternet.com>
2026-09-24 01:12:39 +0100

    Rebuilding handler to manager all server connections including context

--- go/handler.go
+++ go/handler.go
@@ -1,12 +1,22 @@
 package main
 
-import "net/http"
+import(
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 type RecipeHandler struct{
 }
 
-func (rh RecipeHandler) ListRecipes(w http.ResponseWriter, r *http.Request) {}
-func (rh RecipeHandler) GetRecipe(w http.ResponseWriter, r *http.Request) {}
-func (rh RecipeHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
-func (rh RecipeHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
-func (rh RecipeHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
+func (rh *RecipeHandler) ListRecipes(w http.ResponseWriter, *http.Request) {}
+func (rh *RecipeHandler) GetRecipe(w http.ResponseWriter, *http.Request) {}
+func (rh *RecipeHandler) CreateRecipe(w http.ResponseWriter, *http.Request) {}
+func (rh *RecipeHandler) UpdateRecipe(w http.ResponseWriter, *http.Request) {}
+func (rh *RecipeHandler) DeleteRecipe(w http.ResponseWriter, *http.Request) {}
+func (rh *RecipeHandler) RecipeCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "recipeId")
+
+		var err error
+		var recipe Recipe
