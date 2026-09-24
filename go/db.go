9f40869e704634200eba8fc48a736999ea9d4956
Matthew Lowdon-Elia <matt.lowdon@btinternet.com>
2026-09-24 14:48:42 +0100

    Added db file. Currently just []Recipe

---
+++ go/db.go
@@ -0,0 +1,18 @@
+package main
+
+var recipes = []*Recipe{
+	{
+		ID: "1",
+		Name: "Test",
+		Ingredients : []Ingredient{
+			{
+				Name : "Onion",
+				Quantity : 1,
+				Unit : "whole",
+			},
+		},
+		Instructions : []string{"Slice the onion", "Fry the onion"},
+		CookTimeMinutes : 15,
+		Servings : 2,
+	},
+}
