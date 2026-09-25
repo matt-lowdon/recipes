33f209ddcb62ee489078aaaaed995a2a4fde6288
Matthew Lowdon-Elia <matt.lowdon@btinternet.com>
2026-09-25 18:28:04 +0100

    Starting rewrite for SQL init script

--- sql/init.sql
+++ sql/init.sql
@@ -1,22 +1,30 @@
 \c recipes
 
+CREATE TYPE UNIT AS ENUM (
+    'ml',
+    'l',
+    'g',
+    'kg',
+    'tsp',
+    'tbsp',
+    'whole'
+);
+
+CREATE TYPE INGREDIENT AS (
+    name TEXT,
+    quantity INTEGER,
+    unit UNIT
+);
+
 CREATE TABLE IF NOT EXISTS recipes (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
+    ingredients INGREDIENT[] NOT NULL
     servings INTEGER NOT NULL,
     cook_time_minutes INTEGER NOT NULL,
     created_at TIMESTAMPTZ DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS ingredients (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS recipe_ingredients (
-    recipe_id INTEGER NOT NULL REFERENCES recipes(id) ON DELETE CASCADE,
-    ingredient_id INTEGER NOT NULL REFERENCES ingredients(id) ON DELETE CASCADE,
-    quantity NUMERIC NOT NULL,
-    unit TEXT NOT NULL, --g, ml, whole, etc
-    PRIMARY KEY (recipe_id, ingredient_id)
-);
+INSERT INTO recipes VALUES (
+    'Toast',
+    '(Bread, 2, whole)' :: INGREDIENT
